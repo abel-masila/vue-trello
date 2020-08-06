b10f50a39cc99f16d70889933cf6f05837e8f6df
Abel Masila <abelmasila@gmail.com>
2020-08-06 17:12:15 +0300

    worked on Board view and persistent store in localstorage

--- src/views/Board.vue
+++ src/views/Board.vue
@@ -1,9 +1,37 @@
 <template>
-  <div class="board"></div>
+  <div class="board">
+    <div class="flex flexr-row items-start ">
+      <div
+        class="column"
+        v-for="(column, $index) of board.columns"
+        :key="$index"
+      >
+        <div class="flex items-center mb-2 font-bold">
+          {{ column.name }}
+        </div>
+        <div class="list-reset">
+          <div class="task" v-for="task of column.tasks" :key="task.id">
+            <span class="w-full flex-no-shrink font-bold">
+              {{ task.name }}
+            </span>
+            <p
+              class="w-full flex-no-shrink  mt-1 text-sm"
+              v-if="task.description"
+            >
+              {{ task.description }}
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
+import { mapState } from 'vuex'
+export default {
+  computed: mapState(['board'])
+}
 </script>
 
 <style lang="css">
@@ -22,6 +50,6 @@ export default {};
 
 .task-bg {
   @apply pin absolute;
-  background: rgba(0,0,0,0.5);
+  background: rgba(0, 0, 0, 0.5);
 }
 </style>
